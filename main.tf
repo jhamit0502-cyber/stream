41ca03833031438670d30bf3bb83d0f9c11f6826
Amit Jha <jhamit0502@gmail.com>
2026-06-05 21:28:42 +0530

    Update main.tf
    add 1 resource group

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources101"
+  location = "West Europe"
+}
+
